618bea06145cc1c9150d6618641267b4bc27b096
Abhay Kumar <157803348+abhirajpput@users.noreply.github.com>
2024-06-22 19:36:28 +0530

    Add files via upload

---
+++ Datatype.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a = 3;
+    int b = 5;
+
+    int c = a + b;
+
+    cout<< c << endl << "a+b";
+    
+    cout << c;
+    cout << endl;
+    cout << "a + b";
+
+
+    return 0;
+}
