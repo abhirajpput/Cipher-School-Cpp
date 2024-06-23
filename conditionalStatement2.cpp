0cd17afd30ac2e1ee9864a8fcf562357c3a465d3
Abhay Kumar <157803348+abhirajpput@users.noreply.github.com>
2024-06-23 18:23:02 +0530

    Lecture 6

---
+++ conditionalStatement2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//and ,or oprator
+int main(){
+    int marks;
+
+    cout<<"Enter the value of marks : ";
+    cin>>marks;
+
+    if(marks >= 90 && marks <= 100){
+        cout<<"Grade O";
+    }else if(marks >= 80 && marks <= 90){
+        cout<<"Grade A";
+    }else if(marks >= 60 && marks < 80){
+        cout<<"Grade B";
+    }else if(marks >= 40 && marks < 60){
+        cout<<"Grade C";
+    }else if(marks < 40 && marks >= 0){
+        cout<<"Fail";
+    }else{
+        cout<<"Invalid Marks";
+    }
+    
+    return 0;
+}
