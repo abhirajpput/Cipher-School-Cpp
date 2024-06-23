9ac32e8410294b4f7f911cc737e2ecf3f36adbf0
Abhay Kumar <157803348+abhirajpput@users.noreply.github.com>
2024-06-23 18:59:34 +0530

    lecture 11

---
+++ loop3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+//print 10 reverse numbers using while Loop
+int main(){
+    int n = 10;
+    int i = 1;
+    cout<<"n numbers are : ";
+    while(n >= i){
+        cout<<n<<" ";
+        n--;
+    }
+
+    return 0;
+}
