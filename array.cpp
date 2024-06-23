92d9c6a8fb2a29224689b398747780a37afaa334
Abhay Kumar <157803348+abhirajpput@users.noreply.github.com>
2024-06-23 18:35:45 +0530

    lecture 7

---
+++ array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+//array declarations
+
+int main(){
+    int arr[3] = {1,3,4};
+    int arr2[] = {1,2,3,4,5};
+    int arr3[7];
+    int arr4[10] = {0};
+
+    return 0;
+}
